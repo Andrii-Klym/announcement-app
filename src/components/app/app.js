337c2d086d73a67ce3aece89e07b672ae533cea7
Klym Andrii <klym.andriy14@gmail.com>
2022-04-02 18:40:57 +0300

    add changes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import { Component } from 'react/cjs/react.development';
+import { Component } from 'react';
 
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
